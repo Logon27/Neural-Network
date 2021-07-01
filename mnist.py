c0ce9cee31315c595e3ad0883d844ad2457b5641
Logon <32345277+Logon27@users.noreply.github.com>
2021-06-30 22:18:10 -0400

    Changed network model and added visuals
    I changed the network model and added the sigmoid activation function instead. The epochs have been increased to 25 so it will take longer to run overall. I am also training on the entire training set with a learning rate of 0.3. This allows the network to achieve over 90% accuracy in most cases. Additionally I added a performance score compared against a small sample of the test set as well as a visual representation of the actual test set image with the neural network's guess above it. I choose to run it in google colab just for simplicity and computer resources.

--- mnist.py
+++ mnist.py
@@ -2,12 +2,10 @@ import numpy as np
 from keras.datasets import mnist
 from keras.utils import np_utils
 
-from dense import Dense
-from activations import Tanh
-from losses import mse, mse_prime
-
 # load MNIST from server
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# load MNIST copy for image display
+(x_train_image, y_train_image), (x_test_image, y_test_image) = mnist.load_data()
 
 # training data: 60000 samples
 # reshape and normalize input data
@@ -28,20 +26,25 @@ y_test = y_test.reshape(y_test.shape[0], 10, 1)
 
 # neural network
 network = [
-    Dense(28 * 28, 40),
-    Tanh(),
-    Dense(40, 10),
-    Tanh()
+    Dense(28 * 28, 70),
+    Sigmoid(),
+    Dense(70, 35),
+    Sigmoid(),
+    Dense(35, 10),
+    Sigmoid()
 ]
 
-epochs = 100
-learning_rate = 0.1
+epochs = 25
+learning_rate = 0.3
+#not doing anything right now
+num_samples = 1500
 
 # train
 for e in range(epochs):
     error = 0
     # train on 1000 samples, since we're not training on GPU...
-    for x, y in zip(x_train[:1000], y_train[:1000]):
+    #for x, y in zip(x_train[:num_samples], y_train[:num_samples]):
+    for x, y in zip(x_train, y_train):
         # forward
         output = x
         for layer in network:
@@ -55,12 +58,44 @@ for e in range(epochs):
         for layer in reversed(network):
             grad = layer.backward(grad, learning_rate)
 
-    error /= 1000
+    #error /= num_samples
+    error /= x_train.shape[0]
     print('%d/%d, error=%f' % (e + 1, epochs, error))
 
-# test on 20 samples
+# scorecard for how well the network performs, initially empty
+scorecard = []
+
+# go through all the records in the test data set
 for x, y in zip(x_test[:20], y_test[:20]):
     output = x
     for layer in network:
         output = layer.forward(output)
+    prediction = np.argmax(output)
+    correct_label = np.argmax(y)
+    #print("Output: {}".format(output))
     print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))
+    # append correct or incorrect to list
+    if (prediction == correct_label):
+        # network's answer matches correct answer, add 1 to scorecard
+        scorecard.append(1)
+    else:
+        # network's answer doesn't match correct answer, add 0 to scorecard
+        scorecard.append(0)
+    
+# calculate the performance score, the fraction of correct answers
+scorecard_array = np.asarray(scorecard)
+print("performance = ", scorecard_array.sum() / scorecard_array.size)
+
+
+fig, axes = plt.subplots(ncols=20, sharex=False, sharey=True, figsize=(20, 4))
+for i in range(20):
+    output = x_test[i]
+    for layer in network:
+        output = layer.forward(output)
+    prediction = np.argmax(output)
+    
+    axes[i].set_title(prediction)
+    axes[i].imshow(x_test_image[i], cmap='gray')
+    axes[i].get_xaxis().set_visible(False)
+    axes[i].get_yaxis().set_visible(False)
+plt.show()
